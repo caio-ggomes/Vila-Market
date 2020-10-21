6ed03d64ca618aabc16b8e4fe2152cb2c1b750e7
caio-ggomes <caio.graca@gmail.com>
2020-10-20 23:43:06 -0300

    React implementado

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AdvertiseForm from './advertise';
+import Search from './search';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -10,6 +12,20 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AdvertiseForm />
+  </React.StrictMode>,
+  document.getElementById('advertiseForm')
+)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Search />
+  </React.StrictMode>,
+  document.getElementById('search')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
