aa48590c148304e948eaeb1355819511f3d0aab1
Samarasrs <smr.samara.srs@gmail.com>
2020-10-21 21:57:38 -0300

    atualização

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -8,16 +8,11 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <AdvertiseForm />
-  </React.StrictMode>,
-  document.getElementById('advertiseForm')
-)
-
-ReactDOM.render(
-  <React.StrictMode>
     <Search />
   </React.StrictMode>,
-  document.getElementById('search')
-) 
+  document.getElementById('root')
+
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
